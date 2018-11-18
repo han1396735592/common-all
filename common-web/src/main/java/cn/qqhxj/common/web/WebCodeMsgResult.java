f9e20be994ff1aa2df11ddc18177ccadcde21d5a
han1396735592 <1396735592@qq.com>
2018-11-18 14:40:59 +0800

    init common-web module

--- common-web/src/main/java/cn/qqhxj/common/web/WebCodeMsgResult.java
+++ common-web/src/main/java/cn/qqhxj/common/web/WebCodeMsgResult.java
@@ -1,13 +1,51 @@
 package cn.qqhxj.common.web;
 
-import lombok.Data;
-
 /**
  * @author han xinjian
  * @date 2018-11-17 22:52
  **/
-@Data
-public class WebCodeMsgResult<T> extends WebSesult<T> {
+public class WebCodeMsgResult<T> implements WebSesult<T> {
+
     private Integer code;
+
     private String msg;
+
+    private T data;
+
+    private WebCodeMsgResult() {
+
+    }
+
+    public static <T> WebCodeMsgResult<T> ok(T data) {
+        WebCodeMsgResult<T> result = new WebCodeMsgResult<T>();
+        result.data = data;
+        result.code = 0;
+        return result;
+    }
+
+    public static <T> WebCodeMsgResult<T> ok(T data, String msg) {
+        WebCodeMsgResult<T> result = new WebCodeMsgResult<T>();
+        result.data = data;
+        result.code = 0;
+        result.msg = msg;
+        return result;
+    }
+
+    public static <T> WebCodeMsgResult<T> err(String msg) {
+        WebCodeMsgResult<T> result = new WebCodeMsgResult<T>();
+        result.msg = msg;
+        result.code = 500;
+        return result;
+    }
+
+    public static <T> WebCodeMsgResult<T> err(String msg, Integer code) {
+        WebCodeMsgResult<T> result = new WebCodeMsgResult<T>();
+        result.msg = msg;
+        result.code = code;
+        return result;
+    }
+
+    public T getData() {
+        return data;
+    }
 }

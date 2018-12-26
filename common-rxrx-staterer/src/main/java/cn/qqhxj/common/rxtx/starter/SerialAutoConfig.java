01d0b4c1fbb93df65bcd468a530d4e4a69c20689
han1396735592 <1396735592@qq.com>
2018-12-26 10:06:59 +0800

    添加了readme文件

--- common-rxrx-staterer/src/main/java/cn/qqhxj/common/rxtx/starter/SerialAutoConfig.java
+++ common-rxrx-staterer/src/main/java/cn/qqhxj/common/rxtx/starter/SerialAutoConfig.java
@@ -3,6 +3,7 @@ package cn.qqhxj.common.rxtx.starter;
 import cn.qqhxj.common.rxtx.DefaultSerialDataListener;
 import cn.qqhxj.common.rxtx.SerialUtils;
 import cn.qqhxj.common.rxtx.parse.StringSerialDataParser;
+import cn.qqhxj.common.rxtx.processor.SerialByteDataProcesser;
 import cn.qqhxj.common.rxtx.reader.SerialReader;
 import cn.qqhxj.common.rxtx.reader.VariableLengthSerialReader;
 import gnu.io.SerialPort;
@@ -65,6 +66,29 @@ public class SerialAutoConfig {
             e.printStackTrace();
         }
         return serialReader;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SerialByteDataProcesser serialByteDataProcesser() {
+        return new SerialByteDataProcesser() {
+            public void processer(byte[] bytes) {
+                log.debug("接收到数据:{}", bytesToHexString(bytes));
+            }
+
+            public String bytesToHexString(byte[] bArr) {
+                StringBuffer sb = new StringBuffer(bArr.length);
+                String sTmp;
+                for (int i = 0; i < bArr.length; i++) {
+                    sTmp = Integer.toHexString(0xFF & bArr[i]);
+                    if (sTmp.length() < 2) {
+                        sb.append(0);
+                    }
+                    sb.append(sTmp.toUpperCase() + "  ");
+                }
+                return sb.toString();
+            }
+        };
     }
 
     @Bean
